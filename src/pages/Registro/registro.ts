fb48996068eca1f1cf8d81c40f4b17034e232435
MateoViilla <mateoviilla@gmail.com>
2017-07-22 18:02:10 -0500

    mm

--- src/pages/Registro/registro.ts
+++ src/pages/Registro/registro.ts
@@ -3,7 +3,8 @@ import { NavController } from 'ionic-angular';
 import { HomePage } from '../home/home';
 import { CategoriaPage } from '../categoria/categoria';
 import { ToastController } from 'ionic-angular';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';  
+import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';  
+
 
 
 @Component({
@@ -17,10 +18,36 @@ export class RegistroPage {
               public formBuilder: FormBuilder ) {
 
                 this.myForm = this.createMyForm();
+
+              
+
+
+
   }
   
+  private createMyForm(){
+  return this.formBuilder.group({
 
- presentToast() {
+    name: ['', Validators.compose([Validators.maxLength(10),Validators.minLength(2), Validators.pattern('[a-zA-Z ]*'), Validators.required])],
+    email : ['', Validators.compose([Validators.required, Validators.minLength(5), Validators.maxLength(50),Validators.pattern(/^[a-z0-9!#$%&'*+\/=?^_`{|}~.-]+@[a-z0-9]([a-z0-9-]*[a-z0-9])?(\.[a-z0-9]([a-z0-9-]*[a-z0-9])?)*$/i)])],
+    phone: ['', Validators.compose([Validators.maxLength(20),Validators.minLength(5), Validators.pattern('[0-9 ]*'), Validators.required])],
+    dateBirth: ['', Validators.required],
+    passwordRetry: this.formBuilder.group({
+      password: ['', Validators.required],
+      passwordConfirmation: ['', Validators.required]
+    }),
+    gender: ['', Validators.required],
+    magicCode: ['']
+  });
+
+  }
+
+   emailValidator(control: FormControl): {[s: string]: boolean} {
+    if (!(control.value.toLowerCase().match('^[a-zA-Z]\\w*@gmail\\.com$') || control.value.toLowerCase().match('^[a-zA-Z]\\w*@yahoo\\.com$'))) {
+      return {invalidEmail: true};
+    }
+  }
+  presentToast() {
     let toast = this.toastCtrl.create({
       message: 'User was added successfully',
       duration: 3000
@@ -40,22 +67,10 @@ export class RegistroPage {
   saveData(){
     console.log(this.myForm.value);
   }
+  validarContrasena(psw1:string, psw2: string){
 
-  private createMyForm(){
-  return this.formBuilder.group({
-    name: ['', Validators.required],
-    lastName: ['', Validators.required],
-    email: ['', Validators.required],
-    phone: ['', Validators.required],
-    dateBirth: ['', Validators.required],
-    passwordRetry: this.formBuilder.group({
-      password: ['', Validators.required],
-      passwordConfirmation: ['', Validators.required]
-    }),
-    gender: ['', Validators.required],
-    magicCode: ['']
-  });
-}
+  }
+
 
 
   
