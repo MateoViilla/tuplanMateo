72ff2b86d38b48b2fd44b5a7f5c8358c704c2b67
Samuel mejia <Sam@MacBook-Air.local>
2017-07-25 20:19:15 -0500

    cambios en el login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { UsuarioApi } from '../shared/sdk/services';
+import { CategoriaPage } from '../pages/categoria/categoria';
 
 import { InicioPage } from '../pages/inicio/inicio';
 
@@ -9,12 +11,19 @@ import { InicioPage } from '../pages/inicio/inicio';
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = InicioPage;
+  rootPage:any;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(platform: Platform,usuarioApi:UsuarioApi, statusBar: StatusBar, splashScreen: SplashScreen) {
+    let self = this;
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
+      console.log(usuarioApi.isAuthenticated());
+      if (usuarioApi.isAuthenticated()) {
+            self.rootPage = CategoriaPage;
+        } else {
+            self.rootPage = InicioPage;
+        }
       statusBar.styleDefault();
       splashScreen.hide();
     });
