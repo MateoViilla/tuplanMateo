6a861263eeaeb9606fa3509724d616f992fa9fd8
Jose David Hernandez Diaz <jose.k17@hotmail.com>
2017-08-12 00:11:30 -0500

    Validaciones faltantes para las reservas

--- src/pages/modal/modal.ts
+++ src/pages/modal/modal.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams , ViewController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams , ViewController, AlertController } from 'ionic-angular';
 import { UsuarioApi } from '../../shared/sdk/services';
 import { FilterProvider } from '../../providers/filter/filter';
 import { EstablecimientoApi } from '../../shared/sdk/services';
 import { LoopBackConfig } from '../../shared/sdk';
+import { CategoriaPage } from '../categoria/categoria';
 
 
 /**
@@ -19,7 +20,7 @@ import { LoopBackConfig } from '../../shared/sdk';
 })
 export class ModalPage {
   private reserva:any;
-  constructor(public navCtrl: NavController,private filter: FilterProvider, public navParams: NavParams, private view: ViewController, private establecimientoApi: EstablecimientoApi,private usuarioApi: UsuarioApi) {
+  constructor(public alertCtrl:AlertController ,public navCtrl: NavController,private filter: FilterProvider, public navParams: NavParams, private view: ViewController, private establecimientoApi: EstablecimientoApi,private usuarioApi: UsuarioApi) {
  LoopBackConfig.setBaseURL('https://tuplan.herokuapp.com');
     LoopBackConfig.setApiVersion('api');
     this.reserva={};
@@ -28,23 +29,58 @@ export class ModalPage {
     this.reserva.email=""
  }
 mostrar(){
-  this.reserva.nombre=this.usuarioApi.getCachedCurrent().name;
-    this.reserva.telefono=this.usuarioApi.getCachedCurrent().telefono;
-    this.reserva.email=this.usuarioApi.getCachedCurrent().email;
+    if (this.reserva.numeroPersonas == null|| this.reserva.numeroPersona == ""){
+     this.presentAlert("No se pudo completar","Debes llenar todos los campos para poder continuar con tu reserva");
+    } else {
+     var hoy  = new Date();
+     var fechaFormulario = new Date(this.reserva.date);
 
-    this.reserva.establecimiento=this.filter.getEstablecimiento();
-    this.establecimientoApi.nuevaReserva(this.reserva).subscribe((mensaje: any) => {
-      console.log(mensaje);
-alert(mensaje.greeting);    })
+      // Comparamos solo las fechas => no las horas!!
+      hoy.setHours(0,0,0,0);  // Lo iniciamos a 00:00 horas
+      fechaFormulario.setHours(0,0,0,0);
+      if (hoy >= fechaFormulario) {
+        console.log(hoy);
+        console.log(fechaFormulario);
+         this.presentAlert("No se pudo completar","La fecha de reserva debe ser mínimo 24 horas apartir de la fecha actual");
+      }
+      else {
+        console.log("Fecha pasado");
+          console.log(hoy);
+        console.log(fechaFormulario);
+        this.reserva.nombre=this.usuarioApi.getCachedCurrent().name;
+        this.reserva.telefono=this.usuarioApi.getCachedCurrent().telefono;
+        this.reserva.email=this.usuarioApi.getCachedCurrent().email;
 
-    console.log(this.reserva);
+        this.reserva.establecimiento=this.filter.getEstablecimiento();
+        this.establecimientoApi.nuevaReserva(this.reserva).subscribe((mensaje: any) => {
+          console.log(mensaje);
+        alert(mensaje.greeting);    })
+
+        this.toCategoria();
+        this.presentAlert("Reserva exitosa", "Se ha procesado tu solicitud, en unas horas el establecimiento te confirmará la reserva");
+        console.log(this.reserva);
+    }
   }
+}
   ionViewDidLoad() {
     console.log('ionViewDidLoad ModalPage');
   }
 
   closeModal(){
     this.view.dismiss();
+  }
+
+  presentAlert(titulo, mensaje) {
+  let alert = this.alertCtrl.create({
+    title: titulo,
+    subTitle: mensaje,
+    buttons: ['Entendido']
+    
+  });
+  alert.present();
+}
+toCategoria() {
+    this.navCtrl.push(CategoriaPage, { userName: "" });
   }
 
 }
